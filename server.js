3ef51b8e678f3300c120007dbcb2d847130e00d2
tlatlsgksp <130515272+tlatlsgksp@users.noreply.github.com>
2024-03-25 20:24:25 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1538,7 +1538,10 @@ app.post('/lecture_info_select', async (req, res) => {
   if(extra && extra.type === "back_search" && extra.userInput_search && extra.lecture_no_search){
     userInput = extra.userInput_search;
     lecture_no = extra.lecture_no_search;
-  }else{
+  }else if (extra && extra.type === "back_select" && extra.userInput_select && extra.lecture_no_select){
+    userInput = extra.userInput_select;
+    lecture_no = extra.lecture_no_select;
+  } else{
     userInput = req.body.action.params.lecture_name_out_find;
     lecture_no = req.body.action.params.lecture_no;
   }
