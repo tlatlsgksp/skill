bf30e85828246ba976950095e4e276eeb05bb157
tlatlsgksp <130515272+tlatlsgksp@users.noreply.github.com>
2024-04-01 22:05:40 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -3898,12 +3898,17 @@ app.post('/lecture_schedule_edit', async (req, res) => {
         const professors = separatedData.map(data => data[1].replace(/\s+/g, '').toUpperCase());
         const classes = separatedData.map(data => data[2]);
         const places = separatedData.map(data => data[3]);
-        const selectedLectureInfo = lectureList.filter((lecture, index) => {
-          return lecture.과목명 === lectures[index] &&
-              lecture.교수명 === professors[index] &&
-              lecture.분반 === classes[index] &&
-              lecture.강의실 === places[index];
-        });
+        const selectedLectureInfo = lectureList.filter(lecture => {
+          const lectureName = lecture.과목명;
+          const professorName = lecture.교수명;
+          const classNumber = lecture.분반;
+          const place = lecture.강의실;
+      
+          return lectures.includes(lectureName) &&
+              professors.includes(professorName) &&
+              classes.includes(classNumber) &&
+              places.includes(place);
+      });
         console.log(selectedLectureInfo);
         const lectureListText = selectedLectureInfo.map(info => `${info.과목명} - ${info.교수명} - ${info.분반} - ${info.강의실} - ${info.시간표}`).join("\n");
         const text = `시간표에 저장된 강의 목록\n\n${lectureListText}`;
