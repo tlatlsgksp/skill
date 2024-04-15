3be7b0a15a6dd7f7f6a3c10f5d2eb1ad89873d8b
tlatlsgksp <130515272+tlatlsgksp@users.noreply.github.com>
2024-04-16 02:32:50 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -4125,6 +4125,7 @@ app.post('/lecture_schedule_edit', async (req, res) => {
             const place = places[i];
             const lecture = lectureList.find(lecture => 
               lecture.과목명 === lectureName &&
+              lecture.교수명 === professorName &&
               lecture.분반 === classNumber
             );
             if (lecture) {
