cec8bca11d7273ef51e25ae00f49a7fd1bba1414
tlatlsgksp <130515272+tlatlsgksp@users.noreply.github.com>
2024-04-21 07:54:47 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -3456,7 +3456,8 @@ app.post('/lecture_schedule_save', async (req, res) => {
         let text = "수업시간이 겹치는 강의가 있습니다.\n\n";
         for (const overlappingColumn of overlappingColumnsData) {
           const { index, data } = await overlappingColumn;
-          text += `${data.join('')} - ${index}\n`;
+          const [lectures, classes, professors, places] = data.split('\n');
+          text += `${lectures}[${classes}] ${professors} ${places} ${index}\n`;
         }
 
         response = {
